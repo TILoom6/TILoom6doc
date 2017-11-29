2d6dd94ab06c815fdd06338f2b160da541471eb3
yoat <yoatnic@me.com>
2017-11-29 20:43:45 +0900

    5th iteration. FizzBuzz倒した🎉

--- 第２回/2.1/src/main/scala/FizzBuzz.scala
+++ 第２回/2.1/src/main/scala/FizzBuzz.scala
@@ -1,6 +1,7 @@
 object FizzBuzz {
   def convert(number: Int): String = {
     number match {
+      case n if (n % 15 == 0) => "FizzBuzz"
       case n if (n % 3 == 0) => "Fizz"
       case n if (n % 5 == 0) => "Buzz"
       case _ => number.toString
--- 第２回/2.1/src/test/scala/FizzzBuzzSpec.scala
+++ 第２回/2.1/src/test/scala/FizzzBuzzSpec.scala
@@ -5,11 +5,15 @@ class FizzBuzzTypeSpec extends FlatSpec with DiagrammedAssertions {
     assert(FizzBuzz.convert(3) === "Fizz")
   }
 
-  it should "3の倍数ではなかったら'数字'" in {
-    assert(FizzBuzz.convert(2) === "2")
-  }
-
   it should "5の倍数だったら'Buzz'" in {
     assert(FizzBuzz.convert(5) === "Buzz")
+  }
+
+  it should "3と5の倍数だったら'FizzBuzz'" in {
+    assert(FizzBuzz.convert(15) === "FizzBuzz")
+  }
+
+  it should "3もしくは5の倍数ではなかったら'数字'" in {
+    assert(FizzBuzz.convert(2) === "2")
   }
 }
