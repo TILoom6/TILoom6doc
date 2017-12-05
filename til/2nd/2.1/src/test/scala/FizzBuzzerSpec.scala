4c96d2103834f3da3de2b481f0824e88409da175
Pommel Kurama <pommel.pg@gmail.com>
2017-12-06 00:23:00 +0900

    refactor

--- til/2nd/2.1/src/test/scala/FizzBuzzerSpec.scala
+++ til/2nd/2.1/src/test/scala/FizzBuzzerSpec.scala
@@ -1,19 +1,23 @@
+package com.github.tiloom6.tiloom6doc.til.test.fizzbuzz
+
 import org.scalatest._
 
 class FizzBuzzerSpec extends FlatSpec with DiagrammedAssertions {
+  import com.github.tiloom6.tiloom6doc.til.fizzbuzz._
+
   it should "3の倍数だったら'Fizz'" in {
-    assert(FizzBuzzer.convert(3) === "Fizz")
+    assert( FizzBuzzer.convert( 3 ) === "Fizz" )
   }
 
   it should "5の倍数だったら'Buzz'" in {
-    assert(FizzBuzzer.convert(5) === "Buzz")
+    assert( FizzBuzzer.convert( 5 ) === "Buzz" )
   }
 
   it should "3と5の倍数だったら'FizzBuzz'" in {
-    assert(FizzBuzzer.convert(15) === "FizzBuzz")
+    assert( FizzBuzzer.convert( 15 ) === "FizzBuzz" )
   }
 
   it should "3もしくは5の倍数ではなかったら'数字'" in {
-    assert(FizzBuzzer.convert(2) === "2")
+    assert( FizzBuzzer.convert( 2 ) === "2" )
   }
 }
